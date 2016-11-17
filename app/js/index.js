2c123bf05c80a94e4e02d6b05da8f7446debbfd1
Joe Dooley <hello@developingdesigns.com>
2016-11-17 14:35:06 -0500

    initial commit. webpack configured wih babel-polyfill

---
+++ app/js/index.js
@@ -0,0 +1,19 @@
+import jsonData from './data'
+
+/**
+ * The main app object.
+ *
+ */
+const vanillaPress = {
+
+  init () {
+
+    // Add any functions here you want
+    // to run to start the application
+    console.log( jsonData );
+
+  }
+
+};
+
+export default vanillaPress.init()
